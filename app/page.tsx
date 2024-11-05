6848576bdd11e7a5d378f7d1ed21b824e12b6bb0
MarazzaM <marazza.matias@gmail.com>
2024-11-05 17:33:14 +0700

    Fix TypeScript error by using as const type assertion and file separation

--- app/page.tsx
+++ app/page.tsx
@@ -1,64 +1,8 @@
 "use client";
 
 import { zuAuthPopup } from "@pcd/zuauth";
-import type { PipelineEdDSATicketZuAuthConfig } from "@pcd/passport-interface";
 import { useState } from "react";
-
-const ticketTypeNames = [
-  "MegaZu"
-] as const;
-export type TicketTypeName = (typeof ticketTypeNames)[number];
-
-
-const watermark = "0";
-export const whitelistedTickets: Record<
-  TicketTypeName,
-  PipelineEdDSATicketZuAuthConfig[]
-> = {
-  MegaZu: [
-    {
-      "pcdType": "eddsa-ticket-pcd",
-      "publicKey": [
-        "1ebfb986fbac5113f8e2c72286fe9362f8e7d211dbc68227a468d7b919e75003",
-        "10ec38f11baacad5535525bbe8e343074a483c051aa1616266f3b1df3fb7d204"
-      ],
-      "productId": "b6d0715e-27be-5bf2-8041-125cc8e89d07",
-      "eventId": "70848dea-365b-5838-b36e-f691e3151cbd",
-      "eventName": "MegaZu24",
-      "productName": "Resident"
-    },
-    {
-      "pcdType": "eddsa-ticket-pcd",
-      "publicKey": [
-        "1ebfb986fbac5113f8e2c72286fe9362f8e7d211dbc68227a468d7b919e75003",
-        "10ec38f11baacad5535525bbe8e343074a483c051aa1616266f3b1df3fb7d204"
-      ],
-      "productId": "7929010a-d31f-5355-a633-b2e8af4f36db",
-      "eventId": "70848dea-365b-5838-b36e-f691e3151cbd",
-      "eventName": "MegaZu24",
-      "productName": "Core-Organizer"
-    }
-  ]
-};
-const config = Object.entries(whitelistedTickets).flatMap(
-  ([ticketType, tickets]) =>
-    tickets
-      .map((ticket) => {
-        if (ticket.eventId && ticket.productId) {
-          return {
-            pcdType: ticket.pcdType,
-            ticketType: ticketType as TicketTypeName,
-            eventId: ticket.eventId,
-            productId: ticket.productId,
-            eventName: ticket.eventName || "",
-            productName: ticket.productName || "",
-            publicKey: ticket.publicKey
-          };
-        }
-        return null;
-      })
-      .filter((ticket): ticket is NonNullable<typeof ticket> => ticket !== null)
-);
+import { watermark, config } from "./config";
 
 export default function Page() {
   const [pcdData, setPcdData] = useState<string | null>(null);
